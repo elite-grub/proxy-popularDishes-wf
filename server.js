11f5e6882715c12e0c68b08db593f90f271f78d0
Winnie Fong <winniefong21@gmail.com>
2019-03-01 17:47:51 -0800

    Set up server connection

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.listen(port, () => console.log(`Proxy server running from port ${port}!`));
